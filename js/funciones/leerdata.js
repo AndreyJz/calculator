249d7d71cdcd45ebb2f4c1908eb695ee7db86a28
AndreyJz <jerez.01.18@gmail.com>
2024-04-24 22:38:18 -0500

    feat: :tada: first commit

---
+++ js/funciones/leerdata.js
@@ -0,0 +1,34 @@
+const result = document.querySelector(".result")
+const boton = document.querySelectorAll("a");
+
+let numbers = [];
+let operation = null;
+
+boton.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        let number = 0;
+        if (btn.id === "syntax"){
+            result.innerHTML = btn.textContent;
+        } else if (btn.textContent === "C"){
+            numbers = []
+            operation = null
+            result.innerHTML = "0";
+        } else if (btn.id === "op"){
+            number = result.textContent;
+            numbers.push(parseFloat(number));
+            operation = btn.textContent;
+            result.innerHTML = "0";
+        } else {
+            result.innerHTML += btn.textContent;
+        } if (btn.textContent === "="){
+            number = result.textContent;
+            numbers.push(parseFloat(number));
+            result.innerHTML = outputs(operation,numbers);
+        }
+        function outputs (operation,numbers){
+            let expression = numbers.join(operation);
+            let total = eval(expression);
+            return total;
+        }
+    })
+})

e9b87836c60014db930e1c2405d5380d334981f1
AndreyJz <jerez.01.18@gmail.com>
2024-04-25 13:03:38 -0500

    feat: :sparkles: actualizacion js

--- js/funciones/leerdata.js
+++ js/funciones/leerdata.js
@@ -8,13 +8,13 @@ boton.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         let number = 0;
         if (btn.id === "syntax"){
-            if (btn.textContent === Bin){
+            if (btn.textContent === "Bin"){
                 
             } else if (btn.textContent === Oct){
                 
             } else if (btn.textContent === Hex){
                 
-            } else if (btn.textContent === Hex){
+            } else if (btn.textContent === Dec){
                 
             }
             result.innerHTML = btn.textContent;
@@ -25,8 +25,26 @@ boton.forEach((btn) => {
         } else if (btn.id === "op"){
             number = result.textContent;
             numbers.push(parseFloat(number));
-            operation = btn.textContent;
             result.innerHTML = "0";
+            
+            if (btn.textContent === "Sin"){
+                const seno = Math.sin(number * Math.PI/180);
+                result.innerHTML = seno.toFixed(2);
+            } else if (btn.textContent === "Cos"){
+                const coseno = Math.cos(number * Math.PI/180);
+                result.innerHTML = coseno.toFixed(2);
+            } else if (btn.textContent === "Tan"){
+                const tan = Math.tan(number * Math.PI/180);
+                result.innerHTML = tan.toFixed(2);
+            } else if (btn.textContent === "xy" ){
+                operation = "**";
+            } else if (btn.textContent === "√"){
+                const raiz = Math.sqrt(number);
+                result.innerHTML = raiz;
+            } else {
+                operation = btn.textContent;
+            }
+            
         } else {
             result.innerHTML += btn.textContent;
         } if (btn.textContent === "="){
